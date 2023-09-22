f943d59d51bffce9a0fd0c019067dae68656de81
Leul Wujira <meraf000@gmail.com>
2023-09-22 17:51:20 +0300

    feat: add auth slice

---
+++ src/store/auth/slice.js
@@ -0,0 +1,67 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { AUTH } from './types';
+
+const authInitialState = {
+  isLoggedIn: false,
+  user: {
+    data: null,
+    isLoading: false,
+    errors: '',
+  },
+};
+
+export const authSlice = createSlice({
+  name: AUTH,
+  initialState: authInitialState,
+  reducers: {
+    getUserAction: (state) => {
+      state.user.isLoading = true;
+      state.user.errors = '';
+    },
+
+    getUserSuccessAction: (state, { payload: { user } }) => {
+      state.user.data = user;
+      state.user.isLoading = false;
+      state.user.errors = '';
+    },
+
+    loginAction: (state, { payload: { email, password } }) => {
+      state.user.isLoading = true;
+      state.user.errors = '';
+    },
+
+    loginSuccessAction: (state) => {
+      state.isLoggedIn = true;
+      state.user.isLoading = false;
+      state.user.errors = '';
+    },
+
+    createUserAction: (state, { payload: { email, password } }) => {
+      state.user.isLoading = true;
+      state.user.errors = '';
+    },
+
+    createUserSuccessAction: (state, { payload: { user } }) => {
+      state.user.isLoading = false;
+      state.user.data = user;
+      state.user.errors = '';
+    },
+
+    authErrorAction: (state, { payload: { error } }) => {
+      state.user.isLoading = false;
+      state.user.errors = error;
+    },
+  },
+});
+
+export const {
+  getUserAction,
+  createUserAction,
+  loginAction,
+  loginSuccessAction,
+  getUserSuccessAction,
+  createUserSuccessAction,
+  authErrorAction,
+} = authSlice.actions;
+
+export default authSlice.reducer;
