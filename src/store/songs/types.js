ffb2322d7d8fdee6e836d02e381c9f9a52c89052
Leul Wujira <meraf000@gmail.com>
2023-09-22 17:31:21 +0300

    feat: add song action types

---
+++ src/store/songs/types.js
@@ -0,0 +1,7 @@
+export const SONGS = 'SONGS';
+export const GET_SONGS = `${SONGS}/getSongsAction`;
+export const GET_SONG_BY_ID = `${SONGS}/getSongByIdAction`;
+export const SEARCH_SONGS = `${SONGS}/searchSongsAction`;
+export const CREATE_SONG = `${SONGS}/createSongAction`;
+export const UPDATE_SONG = `${SONGS}/updateSongAction`;
+export const DELETE_SONG = `${SONGS}/deleteSongAction`;
