c41fde08f2f6eba65e426f8a0bfadb25a61fcd6e
Leul Wujira <meraf000@gmail.com>
2023-09-22 18:16:43 +0300

    feat: add global style

---
+++ src/components/styles/Button.styled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const StyledButton = styled.button`
+  border: none;
+  border-radius: ${({ theme }) => theme.dimensions.borderRadius};
+  background-color: ${({ theme }) => theme.colors.orange};
+  color: ${({ theme }) => theme.colors.white};
+  padding: 8px 20px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.darkOrange};
+  }
+`;
